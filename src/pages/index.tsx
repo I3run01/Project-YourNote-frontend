dca4f0e69149d6d9b9685e989da40861dd1890f1
I3run01 <brunnooa.v@gmail.com>
2023-04-17 18:32:07 -0300

    c

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,9 @@
-import Head from 'next/head'
 import { InitialStyled } from '../styles/initialStyled'
 
 export default function Home() {
   return (
     <InitialStyled>
-      <Head>
-      </Head>
-
         hello, world!
-
     </InitialStyled>
   )
 }

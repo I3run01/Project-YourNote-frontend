4888a3aca7d057177b0b61448917f71d4b5de228
I3run01 <brunnooa.v@gmail.com>
2023-04-18 18:50:10 -0300

    c

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,9 +1,41 @@
+import Link from 'next/link'
+import { SignInStyled } from '../../styles/signIn'
+import Image from 'next/image'
+import GoogleLogo from '../../../public/images/googleLogo.svg'
+import backButton from '../../../public/images/backButton.svg'
+import { useState } from 'react'
+
+
 const SignUp = () => {
 
     return (
-        <div>
-            SignUp
-        </div>
+        <SignInStyled>
+            <form id='container'>
+                <Link href={'/'} className='backButton'>
+                    <Image
+                        src={backButton}
+                        alt='back button' 
+                    />
+                </Link>
+
+                <input type="text" placeholder='Email' name='Email'               onChange={(event)=>{setEmail(event.target.value)}}/>
+
+                <input type="password" placeholder='Password' 
+                onChange={(event)=>{setPassword(event.target.value)}}/>
+
+                <p id='forgetPassword'>I forget my password</p>
+                
+                <div id="GooglesLogin" >
+                    <Image
+                        src={GoogleLogo}
+                        alt='logo of google'
+
+                        className='googleLogo'
+                    />
+                    <p>Continue with google</p>
+                </div>
+            </form>
+        </SignInStyled>
     )
 }
 

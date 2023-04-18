cb25374c2d201de13babf1b1ee714ea43106e6e0
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-04-17 22:40:44 -0300

    c

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,23 @@
 import { InitialStyled } from '../styles/initialStyled'
 import { InitionalMenu } from '../components/initialMenu'
+import { useSelector, useDispatch } from 'react-redux'
+import { changeTheme } from '../slice/themeSlice'
+import type { RootState } from '../store'
+
+
+
 
 export default function Home() {
+  const theme = useSelector((state: RootState) => state.theme.value)
+  const dispatch = useDispatch()
+
   return (
     <InitialStyled>
       <InitionalMenu/>
+
+      <h1>{theme}</h1>
+
+      <button onClick={() => {theme === 'dark' ? dispatch(changeTheme('light')) : dispatch(changeTheme('dark'))}}></button>
     </InitialStyled>
   )
 }
---
+++ src/slice/themeSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+export interface ThemeState {
+  value: string
+}
+
+const initialState: ThemeState = {
+    value: 'dark',
+}
+
+export const counterSlice = createSlice({
+  name: 'theme',
+  initialState,
+  reducers: {
+    changeTheme: (state, action: PayloadAction<'dark' | 'light'>) => {
+      state.value = action.payload
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const { changeTheme } = counterSlice.actions
+
+export default counterSlice.reducer

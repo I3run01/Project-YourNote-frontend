950d8da3497b93baac2e17c6d064cbe421b3543f
I3run01 <brunnooa.v@gmail.com>
2023-04-18 15:54:40 -0300

    c

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,9 +1,28 @@
+import { SignIn_SignUp } from '../../styles/signIn&signUp'
+import Image from 'next/image'
+import GoogleLogo from '../../../public/images/googleLogo.svg'
+
 const SignIn = () => {
 
     return (
-        <div>
-            SignIn
-        </div>
+        <SignIn_SignUp>
+            <form id='container'>
+                <input type="text" placeholder='Email' name='Email'/>
+                <input type="password" placeholder='Password' />
+
+                <p id='forgetPassword'>I forget my password</p>
+                
+                <div id="GooglesLogin" >
+                    <Image
+                        src={GoogleLogo}
+                        alt='logo of google'
+
+                        className='googleLogo'
+                    />
+                    <p>Continue with google</p>
+                </div>
+            </form>
+        </SignIn_SignUp>
     )
 }
 
---
+++ src/styles/signIn&signUp.tsx
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+
+export const SignIn_SignUp = styled.div`
+    @import url('https://fonts.googleapis.com/css2?family=Dosis:wght@700&display=swap');
+
+    * {
+        font-family: 'Dosis';
+        font-size: 18px;
+    }
+
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+
+    height: 100vh;
+
+    background-image: url('/images/bgLogin.png');
+    background-position: center center;
+    background-size: cover;
+
+    #container {
+        display: flex;
+        flex-direction: column;
+        background-color: blue;
+
+        padding: 40px;
+        border-radius: 10px;
+
+        /* From https://css.glass */
+        background: rgba(255, 255, 255, 0.2);
+        border-radius: 16px;
+        box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+        backdrop-filter: blur(7.9px);
+        -webkit-backdrop-filter: blur(7.9px);
+        border: 1px solid rgba(255, 255, 255, 0.3);
+
+        input {
+            border: none;
+            border-bottom: 1px solid black;
+            margin-bottom: 20px;
+            
+
+            padding: 5px;
+
+            background: none;
+        }
+
+        #forgetPassword {
+            :hover {
+                cursor: pointer;
+                color: blue;
+            }
+        }
+
+        #GooglesLogin {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+            align-items: center;
+            border-radius: 12px;
+            border: 1px solid black;
+            box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+
+            padding: 1px;
+
+            background-color: #d4d4d4;
+
+            :hover {
+                cursor: pointer;
+            }
+
+
+        }
+    }
+
+
+`

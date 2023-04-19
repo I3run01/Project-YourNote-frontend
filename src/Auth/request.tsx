402785e97940fe4d9b96edf1b05fe3dfaacb329c
I3run01 <brunnooa.v@gmail.com>
2023-04-19 17:36:46 -0300

    c

--- src/Auth/request.tsx
+++ src/Auth/request.tsx
@@ -1,1 +1,11 @@
 import axios from "axios"
+
+export const Auth = {
+    apiLink: 'http://localhost:3000',
+
+    signUp: async () => {
+        let ApiRoute = '/register'
+
+        const request = axios.post(this.apiLink)
+    }
+}

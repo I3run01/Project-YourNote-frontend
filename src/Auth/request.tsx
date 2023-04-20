fbea1127c0e658bbf67df6b18b8c626b039b23d9
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-04-20 16:13:18 -0300

    c

--- src/Auth/request.tsx
+++ src/Auth/request.tsx
@@ -16,5 +16,20 @@ export const Auth = {
         } catch(e) {
             return JSON.stringify({message: 'request failed'})
         }
+    },
+
+    signIn: async (email: string, password: string): Promise<string> => {
+        let apiRoute = '/signin'
+
+        try {
+            let response = await axios.post(Auth.apiLink+apiRoute, new URLSearchParams({
+                "email": email,
+                "password": password,
+            }))
+
+            return JSON.stringify(response)
+        } catch(e) {
+            return JSON.stringify({message: 'request failed'})
+        }
     }
 }

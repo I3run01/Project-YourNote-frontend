12f6be98d227e8e6c5bc6e182959e96c03a2d1e7
I3run01 <brunnooa.v@gmail.com>
2023-05-13 12:27:57 -0300

    c

--- src/slice/authSLice.ts
+++ src/slice/authSLice.ts
@@ -19,7 +19,6 @@ export const counterSlice = createSlice({
   },
 })
 
-// Action creators are generated for each case reducer function
 export const { changeAuth } = counterSlice.actions
 
 export default counterSlice.reducer

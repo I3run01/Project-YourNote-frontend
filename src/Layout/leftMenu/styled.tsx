ff3a5fdf79f25a9d092010cda8b9bce6e0d7a8dd
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-05-15 11:25:29 -0300

    c

--- src/Layout/leftMenu/styled.tsx
+++ src/Layout/leftMenu/styled.tsx
@@ -26,15 +26,32 @@ export const LeftMenuDiv = styled.div<props>`
         position: absolute;
         top: 10%;
         right: 0%;
-        background-color: ${props => props.isDark ? bgColorDark : bgColorlight};
-        border: 4px solid ${props => props.isDark ? '#8F8F8F' : '#036D3B'};
 
-        transform: translateX(50%);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        background-color: ${props => props.isDark ? bgColorDark : bgColorlight};
+        border: 3px solid ${props => props.isDark ? '#8F8F8F' : '#036D3B'};
+
+        transform: translateX(50%) ${props => props.isMenuOpened ? 'rotate(0deg)' : 'rotate(180deg)'};
 
         height: 40px;
         width: 40px;
 
         border-radius: 50%;
+
+        transition: all 0.5s ease-in-out;
+        
+        #menuIcon {
+            width: 40%;
+
+            filter: ${props => props.isDark ? 'grayscale(80%)' : ''};
+        }
+
+        :hover {
+            cursor: pointer;
+        }
     }
 
 `

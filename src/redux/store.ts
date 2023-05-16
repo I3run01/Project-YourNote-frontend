bb9a8c216fa964dad7ef42a3816eb200af2f698a
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-05-16 08:33:23 -0300

    c

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,17 +3,12 @@ import themeReducer from './slice/themeSlice'
 import userReducer from './slice/userSlice'
 import createSagaMiddleware from 'redux-saga'
 
-const sagaMiddleware = createSagaMiddleware()
-
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
     user: userReducer
-  },
-  middleware:()=>[sagaMiddleware]
+  }
 })
-
-//sagaMiddleware.run();
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch

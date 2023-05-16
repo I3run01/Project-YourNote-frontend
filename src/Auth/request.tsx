4ad297bc7222ae6caa838d3f1e4f354da3e08365
I3run01 <brunnooa.v@gmail.com>
2023-05-16 14:53:36 -0300

    c

--- src/Auth/request.tsx
+++ src/Auth/request.tsx
@@ -4,8 +4,8 @@ export class Auth {
     private apiLink: string
     
     constructor() {
-        //this.apiLink = 'https://node-user-api.onrender.com/api/users'
-        this.apiLink = 'http://localhost:4000/api/users'
+        this.apiLink = 'https://node-user-api.onrender.com/api/users'
+        //this.apiLink = 'http://localhost:4000/api/users'
     }
 
     async signUp(email: string, password: string): Promise<string> {

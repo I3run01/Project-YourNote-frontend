85fa80a9c743ba2b3c950a97a750f306dc4009c9
Bruno Alves Vieira <brunnooa.v@gmail.com>
2023-05-23 10:04:33 -0300

    v

--- src/components/IDE/index.tsx
+++ src/components/IDE/index.tsx
@@ -1,9 +1,48 @@
-export const IDE = () => {
-    
+// pages/ide.tsx
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+import { Button } from '@material-ui/core';
+import 'brace/mode/javascript';
+import 'brace/theme/monokai';
 
-    return (
-        <div>
+// Dynamic import for react-ace
+const AceEditor = dynamic(
+  async () => {
+    const ace = await import('react-ace');
+    return ace.default;
+  },
+  { ssr: false }
+);
 
-        </div>
-    )
-}
+const Ide = () => {
+  const [code, setCode] = useState('// Write your code here...');
+
+  const handleCopyCode = () => {
+    navigator.clipboard.writeText(code);
+  };
+
+  return (
+    <div>
+      <AceEditor
+        mode="javascript"
+        theme="monokai"
+        name="UNIQUE_ID_OF_DIV"
+        editorProps={{ $blockScrolling: true }}
+        fontSize={14}
+        showPrintMargin={true}
+        showGutter={true}
+        highlightActiveLine={true}
+        value={code}
+        onChange={setCode}
+        setOptions={{
+          showLineNumbers: true,
+          tabSize: 2,
+        }}
+        style={{ width: '100%', height: '70vh' }}
+      />
+      <Button onClick={handleCopyCode}>Copy Code</Button>
+    </div>
+  );
+};
+
+export default Ide;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from 'next/app'
 import { store } from '../redux/store'
 import { Provider } from 'react-redux'
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return(

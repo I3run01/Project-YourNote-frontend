d32699e3d25cf995483f051fb783c86fa1569e9d
I3run01 <brunnooa.v@gmail.com>
2023-05-23 18:16:25 -0300

    c

--- src/components/IDE/index.tsx
+++ src/components/IDE/index.tsx
@@ -1,14 +1,31 @@
-import Editor, { DiffEditor, useMonaco, loader } from '@monaco-editor/react';
+import React, { useRef } from 'react';
+import Editor from '@monaco-editor/react';
+import { editor as EditorType } from 'monaco-editor';
 
-const CodeInputComponent = () => {
+const CodeInputComponent: React.FC = () => {
+  const editorRef = useRef<EditorType.IStandaloneCodeEditor | null>(null);
 
+  const handleEditorDidMount = (
+    editor: EditorType.IStandaloneCodeEditor,
+  ) => {
+    editorRef.current = editor;
+  };
+
+  const showValue = () => {
+    if (editorRef.current) {
+      alert(editorRef.current.getValue());
+    }
+  };
 
   return (
     <div>
-        <Editor 
-            height="90vh"
-            defaultLanguage="python" 
-            defaultValue="//put some code" />;
+      <Editor
+        height="90vh"
+        defaultLanguage="python"
+        defaultValue="// some comment"
+        onMount={handleEditorDidMount}
+      />
+      <button onClick={showValue}>Show Code</button>
     </div>
   );
 };

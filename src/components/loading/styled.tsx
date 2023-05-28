b3a8f398cb14382ee4d3626ea6a59806892c7a6b
I3run01 <brunnooa.v@gmail.com>
2023-05-28 10:24:32 -0300

    loading component correction

--- src/components/loading/styled.tsx
+++ src/components/loading/styled.tsx
@@ -4,14 +4,16 @@ export const LoadingDiv = styled.div`
   position: fixed;
   top: 0;
   left: 0;
+  right: 0;
+  bottom: 0;
+
 
   z-index: 10;
   
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
-  width: 100vw;
+
 
   #Spinner {
      @keyframes spin {

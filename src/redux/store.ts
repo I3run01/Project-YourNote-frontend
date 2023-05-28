134872ebb348d2dfbd2083b135165fbeb15a77ed
I3run01 <brunnooa.v@gmail.com>
2023-05-28 07:35:07 -0300

    file redux deleted

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
 import themeReducer from './slice/themeSlice'
 import userReducer from './slice/userSlice'
-import fileReducer from '../redux/slice/fileSlice'
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
     user: userReducer,
-    file: fileReducer
   }
 })
 
